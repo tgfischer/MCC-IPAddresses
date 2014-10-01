8338bd515ededa1f115f6c5df3892485985e820f
Tom Fischer <tfische5@uwo.ca>
2014-10-01 11:45:24 -0400

    Added some documentation

--- IP.java
+++ IP.java
@@ -1,26 +1,31 @@
 package ip_addresses;
 
 public class IP {
-	public int[] nums;
+	public int[] nums;										// Stores the different parts of the IP address
 	
 	public IP() {
-		nums = new int[4];
+		this.nums = new int[4];
 	}
 	
+	/**
+	 * Used to set the values of the IP address
+	 * @param ip - A string containing the 4 parts of the IP address, separated by periods (.)
+	 * @return Whether or not the IP is valid
+	 */
 	public boolean setIP(String ip) {
-		String[] values = ip.split("\\.");
+		String[] values = ip.split("\\.");					// Split the IP at the periods and store the items in an array
 		
-		if (values.length != 4) return false;
+		if (values.length != 4) return false;				// Return false if there are not 4 parts in the array
 		
-		for (int i = 0; i < values.length; i++) {
-			int value = Integer.parseInt(values[i]);
+		for (int i = 0; i < values.length; i++) {			// Iterate through all the parts of the IP address
+			int value = Integer.parseInt(values[i]);		// Convert the part to an integer
 			
-			if (value < 0 || value > 255) return false;
+			if (value < 0 || value > 255) return false;		// Check if this part is less than 0 or greater than 255
 			
-			nums[i] = value;
+		this.nums[i] = value;								// Save this part to this objects IP address
 		}
 		
-		return true;
+		return true;										// Return that this IP is valid
 	}
 
 }
--- Main.java
+++ Main.java
@@ -6,43 +6,51 @@ import java.io.FileReader;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			// This opens the JudgeInput text file for reading
 			BufferedReader reader = new BufferedReader(new FileReader("JudgeInput.txt"));
 			
-			String line;
+			String line;									// Stores the line that is read from the text file
 			
-			while ((line = reader.readLine()) != null) {
-				String[] ips = line.split(" ");
+			while ((line = reader.readLine()) != null) {	// Iterates through all of the lines in the text file
+				String[] ips = line.split(" ");				// Splits the line at each space, and save the items in an array
 				
-				IP start = new IP();
-				start.setIP(ips[0]);
+				IP start = new IP();				// Create a new start IP object
+				start.setIP(ips[0]);				// Set up the underlying array in the IP object
 				
-				IP end = new IP();
-				end.setIP(ips[1]);
+				IP end = new IP();					// Create a new end IP object
+				end.setIP(ips[1]);					// Set up the underlying array in the IP object
 				
-				IP check = new IP();
+				IP check = new IP();				// Create a new IP that you will be checking against the start and end IPs
 				
-				if (!check.setIP(ips[2])) {
-					System.out.println("InValid");
-					continue;
+				if (!check.setIP(ips[2])) {			// Set up the underlying array in the IP object, and check if the IP is valid
+					System.out.println("InValid");	// Print out that the IP is invalid
+					continue;						// Skip to the next set of IPs
 				}
 				
-				boolean output = checkInRange(start, end, check);
-				if (output) System.out.println("InRange");
-				else System.out.println("OutRange");
+				if (checkInRange(start, end, check)) System.out.println("InRange");		// If the IP that you are checking is in the subnet
+				else 								 System.out.println("OutRange");	// If the IP that you are checking is not in the subnet
 			}
 			
-			reader.close();
-		} catch (Exception e) {
+			reader.close();		// Close the buffered reader
+		} catch (Exception e) { // Catches all exceptions (Was easier since I was pressed for time...)
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Checks to see if the 'check' IP address is in the subnet (defined by the start IP and the end IP)
+	 * @param start - the lower bound for the subnet range
+	 * @param end   - the upper bound for the subnet range
+	 * @param check - the IP you are checking
+	 * @return Whether or not the IP is in the subnet
+	 */
 	public static boolean checkInRange(IP start, IP end, IP check) {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++) { 	// Iterates through the 4 parts of the IP addresses
+			// Checks if this part of the IP is in the range, and it is not 0
 			if ((check.nums[i] < start.nums[i] || check.nums[i] > end.nums[i]) && check.nums[i] != 0) 
-				return false; 
+				return false; 			// Return that this IP is not in the subnet
 		}
 		
-		return true;
+		return true;					// Return that this IP is in the subnet
 	}
 }
